a8c9e31ffc9a6df2e07bc445d4fcb49778a8c1ae
josepablovr <josepablovasro@gmail.com>
2025-01-29 11:29:21 +0100

    Updated RoboTeQ Driver to ROS2

--- robo_base/include/robo_base/RoboteqDevice.h
+++ robo_base/include/robo_base/RoboteqDevice.h
@@ -1,12 +1,11 @@
 #ifndef __RoboteqDevice_H_
 #define __RoboteqDevice_H_
 
-using namespace std;
 #include <vector>
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Twist.h"
-#include "robo_base/robo_io.h"
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include "geometry_msgs/msg/twist.hpp"
+#include "robo_io/msg/robo_io.hpp"
 
 using namespace std;
 
@@ -16,13 +15,16 @@ void sleepms(int milliseconds);
 class RoboteqDevice
 {
 private:
-	int device_fd;
+    int device_fd;
 	int fd0;
 	int handle;
-	ros::Subscriber cmd_sub, subenc, subsys;
+
+    // Subscribers
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_sub_;
+    rclcpp::Subscription<robo_io::msg::RoboIO>::SharedPtr cmd_io_sub_;
 
 protected:
-	void InitPort();
+    void InitPort();
 
 	int Write(string str);
 	int ReadAll(string &str);
@@ -31,29 +33,29 @@ protected:
 	int IssueCommand(string commandType, string command, int waitms, string &response, bool isplusminus = false);
 
 public:
-	bool IsConnected();
-	int Connect(string port);
-	void Disconnect();
+    bool IsConnected();
+    int Connect(string port);
+    void Disconnect();
 
-	int SetConfig(int configItem, int index, int value);
-	int SetConfig(int configItem, int value);
+    int SetConfig(int configItem, int index, int value);
+    int SetConfig(int configItem, int value);
 
-	int SetCommand(int commandItem, int index, int value);
-	int SetCommand(int commandItem, int value);
-	int SetCommand(int commandItem);
+    int SetCommand(int commandItem, int index, int value);
+    int SetCommand(int commandItem, int value);
+    int SetCommand(int commandItem);
 
-	int GetConfig(int configItem, int index, int &result);
-	int GetConfig(int configItem, int &result);
+    int GetConfig(int configItem, int index, int &result);
+    int GetConfig(int configItem, int &result);
 
-	int GetValue(int operatingItem, int index, std::string &result);
-	int GetValue(int operatingItem, std::string &result);
+    int GetValue(int operatingItem, int index, std::string &result);
+    int GetValue(int operatingItem, std::string &result);
 
-	void cmdCallback(const geometry_msgs::Twist::ConstPtr& vel);
-        void cmd_ioCallback(const robo_base::robo_io::ConstPtr& robo);
-	int MixedModeMotorMove(float throttle, float steering, string &response);
+    void cmdCallback(const geometry_msgs::msg::Twist::SharedPtr vel);
+    void cmd_ioCallback(const robo_io::msg::RoboIO::SharedPtr robo);
+    int MixedModeMotorMove(float throttle, float steering, string &response);
 
-	RoboteqDevice();
-	~RoboteqDevice();
+    RoboteqDevice();
+    ~RoboteqDevice();
 };
 
 #endif
